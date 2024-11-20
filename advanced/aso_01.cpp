7f87ddb78123020429d71afbcc70d4b75a361a75
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2024-11-20 15:50:29 +0300

    Update aso_01.cpp

--- advanced/aso_01.cpp
+++ advanced/aso_01.cpp
@@ -5,13 +5,13 @@ template <typename T, typename U>
 class Nec {
 private:
 	T mt;
-	T mu;
+	U mu;
 public:
 	//...
 	auto operator<=>(const Nec& other)->std::common_comparison_category_t<decltype(mt <=> mt), decltype(mu <=>mu)>
 	{
-		if (auto cmp1 = mt <=> other.mt; cmp1 != 0)
-			return cmp1;
+		if (auto cmp = mt <=> other.mt; cmp != 0)
+			return cmp;
 
 		return mu <=> other.mu;
 	}
