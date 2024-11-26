a89e966be0fc751f23ccbccd549a2a271dcdf8e0
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2024-11-26 12:49:30 +0300

    Create custom_types_12.cpp

---
+++ custom_types_12.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <compare>
+
+template<typename T, typename U>
+auto CompareStrongOrderFallback(const T& lhs, const U& rhs)
+{
+	if constexpr (std::three_way_comparable_with<T, U>)
+	{
+		return lhs <=> rhs;
+	}
+	else
+	{
+		if (lhs == rhs)
+			return std::strong_ordering::equal;
+		if (lhs < rhs)
+			return std::strong_ordering::less;
+
+		return std::strong_ordering::greater;
+	}
+}
+
+class identity
+{
+public:
+	bool operator<(const identity&) const;
+	bool operator==(const identity&) const;
+};
+
+class Person
+{
+	identity m_id;
+	int m_no;
+	std::strong_ordering operator <=>(const Person& other)const
+	{
+		if (auto result = m_no <=> other.m_no; result != 0)
+			return result;
+		return CompareStrongOrderFallback(m_id, other.m_id);
+	}
+};
