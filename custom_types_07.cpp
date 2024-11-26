a7ea3beec673d1c2b7dbea7bd464533186a8c702
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2024-11-26 12:37:17 +0300

    Create custom_types_07.cpp

---
+++ custom_types_07.cpp
@@ -0,0 +1,25 @@
+// quoted from the book C++20 - Jossutis
+
+#include <string>
+#include <cassert>
+
+class Person {
+	std::string name;
+	double value;
+public:
+	std::strong_ordering operator<=> (const Person& rhs) const
+	{
+		auto cmp1 = name <=> rhs.name;
+		if (cmp1 != 0)
+			return cmp1; // return strong_ordering for std::string
+
+		auto cmp2 = value <=> rhs.value; // might be partial_ordering for double
+		// map partial_ordering to strong_ordering:
+		
+		assert(cmp2 != std::partial_ordering::unordered); // RUNTIME ERROR if unordered
+		
+		return	cmp2 == 0 ? std::strong_ordering::equal :
+			   cmp2 > 0 ? std::strong_ordering::greater :
+				std::strong_ordering::less;
+	}
+};
